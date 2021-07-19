058a9a6cf860822c12b1dc655b2ec02ada168ec9
gocheat <itsinil@gmail.com>
2021-07-19 23:29:12 +0900

    init project 넷플릭스 클론 코딩

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screens/home_screen.dart';
+import 'package:netflix_clone/widgets/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController _controller;
+
+  @override
+  Widget build(BuildContext contet) {
+    return MaterialApp(
+        title: 'BbongFlix',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            accentColor: Colors.white),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: SafeArea(
+              child: TabBarView(
+                // 탭 화면을 마우스 모션으로 변경하지 않겠다는 의미
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  HomeScreen(),
+                  Container(
+                    child: Center(
+                      child: Text('search'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('save'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('more'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            bottomNavigationBar: BottomBar(),
+          ),
+        ));
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/model/move_model.dart';
+
+// 영화 정보, 미리보기 화면
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<Movie> movies = [
+    Movie.formMap({
+      'title': '상견리',
+      'keyword': '사랑/판타지/로맨스',
+      'poster': 'images/mw2lS4rjo5lt4v2L2tI6xaVJCgM.jpeg',
+      'like': false,
+    })
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar();
+  }
+}
+
+class TabBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'images/netflix-logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
