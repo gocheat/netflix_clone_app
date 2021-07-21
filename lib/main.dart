00d5d251ca4aa86aa5c11cf6aae19219a416f1f5
gocheat <itsinil@gmail.com>
2021-07-21 23:27:00 +0900

    fix: SafeArea 조정

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext contet) {
     return MaterialApp(
-        title: 'BbongFlix',
+        title: 'Netflix Clone',
         theme: ThemeData(
             brightness: Brightness.dark,
             primaryColor: Colors.black,
@@ -24,27 +24,29 @@ class _MyAppState extends State<MyApp> {
         home: DefaultTabController(
           length: 4,
           child: Scaffold(
-            body: TabBarView(
-              // 탭 화면을 마우스 모션으로 변경하지 않겠다는 의미
-              physics: NeverScrollableScrollPhysics(),
-              children: [
-                HomeScreen(),
-                Container(
-                  child: Center(
-                    child: Text('search'),
+            body: SafeArea(
+              child: TabBarView(
+                // 탭 화면을 마우스 모션으로 변경하지 않겠다는 의미
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  HomeScreen(),
+                  Container(
+                    child: Center(
+                      child: Text('search'),
+                    ),
                   ),
-                ),
-                Container(
-                  child: Center(
-                    child: Text('save'),
+                  Container(
+                    child: Center(
+                      child: Text('save'),
+                    ),
                   ),
-                ),
-                Container(
-                  child: Center(
-                    child: Text('more'),
+                  Container(
+                    child: Center(
+                      child: Text('more'),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             bottomNavigationBar: BottomBar(),
           ),
